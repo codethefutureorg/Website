828c488345cbaeb30f894c00e7895540f12dc643
Chloe Yan <chloeyan@MacBook-Pro-2.localdomain>
2021-08-09 17:59:51 -0700

    Add email collection to every page

--- script.js
+++ script.js
@@ -32,3 +32,26 @@ window.onclick = function(e) {
     }
   }
 }
+
+// Email collection
+window.onload = () => {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js';
+  document.body.appendChild(script);
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxkYPn2VevwT4e5_YPN6puJspYEL_udrNJ9v5Bsa3AFJRhIlQ3v/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+      var button = document.getElementById("submit");
+      button.innerHTML = "You're added! ✓";
+      button.style.border = "2px solid #0995E3";
+      button.style.backgroundColor = "#0995E3";
+      button.disabled = true;
+  })
+}
